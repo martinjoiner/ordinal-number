9362295cd1218a916d274e8f4453216f7c2c3a00
Martin Joiner <martin@martinjoiner.co.uk>
2016-02-06 20:37:26 +0000

    Fixed typo in namespace

--- src/OrdinalNumber.php
+++ src/OrdinalNumber.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace \MartinJoiner\OrdinalNumber;
+namespace MartinJoiner\OrdinalNumber;
 
 /** 
  * Takes a number in the range 1-9999 and converts it to a human readable sentence of it's ordinal form eg. 'Three thousand five hundred sixty first'
